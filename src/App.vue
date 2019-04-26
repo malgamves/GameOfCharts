7d3864b9ae499f7c0551dca16df0ed4a21a1cb39
Daniel Phiri <malgamves@gmail.com>
2019-04-26 14:40:04 +0530

    design changes

--- src/App.vue
+++ src/App.vue
@@ -2,110 +2,145 @@
   <div id="app">
     <div class="container">
       <div class="row">
-        <div class="column">
-          <ul v-for="charName of characters" 
-          v-bind:key="charName.id">
-          <button @click="updateVotes(charName.id)">{{charName.name}}
-            </button>
-          </ul>
-        </div>
-        <div class="column chart">
-          <div class="row">
             <div class="column">
-          <h2>Who Might Die ⚔️</h2>
+              <h2>Who Might Die ⚔️</h2>
             </div>
             <div class="column">
-              <h2 v-if="loading">⚖️ Total Votes: {{totalVotes.characters_aggregate.aggregate.sum.votes}}</h2>
+              <h2
+                v-if="loading"
+              >⚖️ Total Votes: {{totalVotes.characters_aggregate.aggregate.sum.votes}}</h2>
             </div>
           </div>
-          <commit-chart 
-          v-if="loaded"
-          :chartData="chartData" />
-       </div>
+      <div class="button-box">
+        <div v-for="charName of characters" v-bind:key="charName.id">
+          <button class="button" @click="updateVotes(charName.id)">{{charName.name}}</button>
+        </div>
+      </div>
+      <div>
+        <div class="chart">
+          
+          <commit-chart v-if="loaded" :chartData="chartData" :options="options" :width="200" :height="300"/>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import CommitChart from './components/CommitChart.js'
-import { ALL_CHAR_QUERY, ADD_VOTE_MUTATION, ALL_VOTES_SUBSCRIPTION, SUM_VOTES_SUBSCRIPTION } from './constants/graphql'
-import graphql2chartjs from 'graphql2chartjs';
+import CommitChart from "./components/CommitChart.js";
+import {
+  ALL_CHAR_QUERY,
+  ADD_VOTE_MUTATION,
+  ALL_VOTES_SUBSCRIPTION,
+  SUM_VOTES_SUBSCRIPTION
+} from "./constants/graphql";
+import graphql2chartjs from "graphql2chartjs";
 
 export default {
-  name: 'app',
+  name: "app",
   props: {
-    msg: String,
+    msg: String
   },
-  data(){
+  data() {
     return {
       loaded: false,
       loading: false,
-      characters: '',
+      datacollection: {
+          //Data to be represented on x-axis
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              pointBackgroundColor: 'white',
+              borderWidth: 1,
+              pointBorderColor: '#249EBF',
+              //Data to be represented on y-axis
+              data: [40, 20, 30, 50, 90, 10, 20, 40, 50, 70, 90, 100]
+            }
+          ]
+        },
+      characters: "",
       totalVotes: 0,
       chartData: null,
       options: {
-            responsive: true,
-            maintainAspectRatio: false
-                },
-    }
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: false
+              }
+            }],
+            xAxes: [ {
+              gridLines: {
+                display: true
+              }
+            }]
+          },
+          legend: {
+            display: true
+          },
+          responsive: true,
+          maintainAspectRatio: true
+        }
+    };
   },
   components: {
     CommitChart
   },
   methods: {
-    updateVotes(id){
+    updateVotes(id) {
       this.$apollo.mutate({
-
         mutation: ADD_VOTE_MUTATION,
 
         variables: {
-          id: id,
+          id: id
         }
       });
     }
   },
   apollo: {
-      characters: {
-        query: ALL_CHAR_QUERY
+    characters: {
+      query: ALL_CHAR_QUERY
+    },
+    $subscribe: {
+      votes: {
+        query: ALL_VOTES_SUBSCRIPTION,
+        result({ data }) {
+          const g2c = new graphql2chartjs();
+          g2c.add(data, "bar");
+          this.chartData = g2c.data;
+          this.loaded = true;
+          //console.log(g2c.data)
+        }
       },
-      $subscribe: {
-        votes: {
-          query: ALL_VOTES_SUBSCRIPTION,
-          result({data}) {
-            const g2c = new graphql2chartjs();
-            g2c.add(data, 'bar')
-            this.chartData = g2c.data
-            this.loaded = true
-            //console.log(g2c.data)
-          }
-        },
-        sumVotes: {
-          query: SUM_VOTES_SUBSCRIPTION,
-          result({data}) {
-            if(data){
+      sumVotes: {
+        query: SUM_VOTES_SUBSCRIPTION,
+        result({ data }) {
+          if (data) {
             this.totalVotes = data;
             this.loading = true;
             //console.log(data);
-            }
           }
         }
-      },
+      }
     }
-}
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 1%;
 }
 .container {
-  margin:  0 auto;
+  margin: 0 auto;
 }
 
 .row {
@@ -114,13 +149,29 @@ export default {
 
 .column {
   flex: 50%;
+  text-align: center;
 }
 
 .chart {
-    padding: 20px;
-    box-shadow: 0px 0px 20px 2px rgba(0, 0, 0, .4);
-    border-radius: 20px;
-    float: right;
-  }
+  box-shadow: 0px 0px 20px 2px rgba(0, 0, 0, 0.4);
+  border-radius: 10px;
+  padding-top: 15%;
+}
+
+button {
+  background-color:black;
+  border-radius: 10px;
+  color: white ;
+  width: 100px;
+  height: 40px;
+  float: left;
+  margin: 2%;
+  text-align: center;
+}
+
+.button-box {
+  text-align: center;
+
+}
 </style>
 
